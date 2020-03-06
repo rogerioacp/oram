f1f8a964038cca0bfdfc4b6d4283c1cbcc992614
Rogério Pontes <rogerio.pontes@pm.me>
2020-03-06 09:01:36 +0000

    update token size

--- src/tests/tpmap.c
+++ src/tests/tpmap.c
@@ -51,12 +51,11 @@ int test(size_t nblocks, size_t blockSize, size_t bucketCapcity, size_t nwrites)
     int index = 0;
     int readi = 0;
     char *data = NULL;
-    unsigned int token[32];
+    unsigned int token[4];
 
     unsigned int nextLeaf;
     unsigned int nextPartition;
     
-
     //printf("Going to init\n");
     //malloc input is size in bytes. sizeof gives size in bytes.
     char **strings = (char **) malloc(sizeof(char *) * nblocks);
